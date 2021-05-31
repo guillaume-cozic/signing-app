055f445f1b8a6b98a6a8986209f387b291bcf4d4
Guillaume COZIC <guillaume.cozic@gmail.com>
2021-05-31 09:47:50 +0200

    fix affichage page

--- resources/views/boattrips/datatable.blade.php
+++ resources/views/boattrips/datatable.blade.php
@@ -3,12 +3,20 @@
         <thead>
              <tr role="row">
                 <th data-priority="35">
-                    <div class="d-none d-sm-block"><i class="fa fa-ship"></i>Bateaux</div>
+                    <div class="d-none d-sm-block">
+                        <i class="fa fa-ship"></i>Bateaux
+                    </div>
+                    <div class="d-block d-sm-none">
+                        Bateaux
+                    </div>
                 </th>
                 <th data-priority="30" class="desktop"> Total</th>
                 <th data-priority="10">
                     <div class="d-none d-sm-block">
                         <i class="fa fa-anchor"></i> Marin
+                    </div>
+                    <div class="d-block d-sm-none">
+                        Marin
                     </div>
                 </th>
                 <th data-priority="50">Départ</th>
@@ -16,6 +24,9 @@
                 <th data-priority="20">
                     <div class="d-none d-sm-block">
                         <i class="fa fa-cogs"></i> Actions
+                    </div>
+                    <div class="d-block d-sm-none">
+                        Actions
                     </div>
                 </th>
             </tr>
